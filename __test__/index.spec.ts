737ec2f19a04c1229038742f19c413082f06d66c
SHIROHA <shir0ha@qq.com>
2024-06-20 09:55:06 +0800

    #FIX: eslint error

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -4,11 +4,8 @@ import { startListener } from '../index'
 
 test('sync function from native code', (t) => {
   t.pass()
-  console.log(1233333)
   startListener((data) => {
     const dd = JSON.parse(data)
     dd.data = JSON.parse(dd.data)
-    console.log('123', dd)
   })
-  console.log(1232222)
 })
